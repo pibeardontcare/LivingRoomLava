4d203121aa2d3f02a2638c7c321a6cd21a61f3bd
pibeardontcare <howtopoz@gmail.com>
2023-06-27 20:34:45 -0700

    Debugging

--- Assets/PillowTracker.cs
+++ Assets/PillowTracker.cs
@@ -1,37 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PillowTracker : MonoBehaviour
 {
-
-
-    // Start is called before the first frame update
     public List<GameObject> pillows; // List to hold the pillow game objects
     public LineRenderer lineRenderer; // Reference to the LineRenderer component
+    public Text infoText; // Reference to the Text component for displaying the information
 
-    // Start is called before the first frame update
     void Start()
     {
         pillows = new List<GameObject>();
         lineRenderer = GetComponent<LineRenderer>(); // Get reference to the LineRenderer component attached to this object
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // Clear the line renderer
-        lineRenderer.positionCount = 0;
+        lineRenderer.positionCount = 0; // Clear the line renderer
 
-        // Iterate over the pillows list and store the position data
         foreach (GameObject pillow in pillows)
         {
-            // Store the position data of each pillow
             Vector3 position = pillow.transform.position;
 
-            // Add the position to the line renderer
             lineRenderer.positionCount++;
             lineRenderer.SetPosition(lineRenderer.positionCount - 1, new Vector3(position.x, 0f, position.z));
         }
+
+        PrintPillowInformation(); // Print the pillow information
+    }
+
+    void PrintPillowInformation()
+    {
+        string info = "Pillow Information:\n";
+
+        foreach (GameObject pillow in pillows)
+        {
+            Vector3 position = pillow.transform.position;
+            Vector3 dimensions = pillow.transform.localScale;
+
+            info += "Position: " + position + ", Dimensions: (x: " + dimensions.x + ", z: " + dimensions.z + ")\n";
+        }
+
+        infoText.text = info; // Update the Text component with the pillow information
     }
 }
