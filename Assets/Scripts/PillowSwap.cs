d7b7a8e5b738ad04d902cb04ef8d0035ff1b590c
pibeardontcare <howtopoz@gmail.com>
2023-07-11 22:28:52 -0700

    Avoids overlapping interactables

--- Assets/Scripts/PillowSwap.cs
+++ Assets/Scripts/PillowSwap.cs
@@ -15,11 +15,30 @@ public class PillowSwap : MonoBehaviour
             // Set the y position to 0.1
             objectPosition.y = 0.1f;
 
-            // Instantiate the safe area object with zero rotation and modified position
-            GameObject newSafeArea = Instantiate(safeAreaPrefab, objectPosition, Quaternion.identity);
+            // Check for overlapping objects
+            bool isOverlapping = CheckOverlap(objectPosition);
 
-            // Destroy the original object
-            Destroy(gameObject);
+            if (!isOverlapping)
+            {
+                // Instantiate the safe area object with zero rotation and modified position
+                GameObject newSafeArea = Instantiate(safeAreaPrefab, objectPosition, Quaternion.identity);
+
+                // Destroy the original object
+                Destroy(gameObject);
+            }
+            else
+            {
+                Debug.Log("Object overlap detected. Cannot spawn new object.");
+            }
         }
+    }
+
+    private bool CheckOverlap(Vector3 position)
+    {
+        // Get all colliders within the safe area's position and size
+        Collider[] colliders = Physics.OverlapBox(position, safeAreaPrefab.transform.localScale / 2f);
+
+        // Check if any colliders were found (excluding self)
+        return colliders.Length > 0;
     }
 }
