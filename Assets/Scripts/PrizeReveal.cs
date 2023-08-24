ae6e2b92caaacfe577f27bab1b6263f9896a79bb
pibeardontcare <howtopoz@gmail.com>
2023-08-23 20:47:46 -0700

    Animation updates & lamp physics

--- Assets/Scripts/PrizeReveal.cs
+++ Assets/Scripts/PrizeReveal.cs
@@ -5,10 +5,11 @@ public class PrizeReveal : MonoBehaviour
 {
     public ColorChanger colorChanger; // Drag your ColorChanger script here
     public GameObject prizeDescriptionUI;
-   
+
     public ParticleSystem particleEmitter; // Drag your particle emitter here
     public Animator endSequenceAnimator; // Drag your end sequence animator here
 
+    [SerializeField] private Animator prizeBox;
     private bool triggerEntered = false; // Flag to track if the trigger has been entered
 
     private void Start()
@@ -18,6 +19,7 @@ public class PrizeReveal : MonoBehaviour
         {
             particleEmitter.Stop();
         }
+        prizeBox.SetBool("PrizeReveal", false); // Fixed missing semicolon and added the missing parameter
     }
 
     private IEnumerator PlayEndSequenceWithDelay(float delay)
@@ -31,10 +33,11 @@ public class PrizeReveal : MonoBehaviour
         }
     }
 
-        private void OnTriggerEnter(Collider other)
-        {
+    private void OnTriggerEnter(Collider other)
+    {
         if (other.CompareTag("Player") && !colorChanger.IsGameOver)
         {
+            prizeBox.SetBool("PrizeReveal", true);
             // Enable prize description UI and next level button if it's not already active
             if (!prizeDescriptionUI.activeSelf)
             {
@@ -52,6 +55,5 @@ public class PrizeReveal : MonoBehaviour
             float delayBeforeEndSequence = 2.0f; // Adjust the delay time as needed
             StartCoroutine(PlayEndSequenceWithDelay(delayBeforeEndSequence));
         }
-        }
-
+    }
 }
