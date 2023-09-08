02cb9032697b9be916b57c53bc3e7e6f1f005bb5
pibeardontcare <howtopoz@gmail.com>
2023-09-07 20:36:08 -0700

    end sequence testing

--- Assets/EndSequence.cs
+++ Assets/EndSequence.cs
@@ -1,25 +1,63 @@
 using UnityEngine;
 
-public class TriggerScript : MonoBehaviour
-{
-    public Animator animator; // Drag and drop the Animator component from the Inspector.
-    public ParticleSystem particleEmitter; // Drag and drop the Particle System component from the Inspector.
+public class ShowHideObject : MonoBehaviour
+{public GameObject objectToShowHide;
+    public ParticleSystem particleEmitter;
+    public Camera oculusMainCamera;
 
-    private void OnTriggerEnter(Collider other)
+    // Define x and z boundaries for the object
+    public float minX;
+    public float maxX;
+    public float minZ;
+    public float maxZ;
+
+    private bool isInsidePerimeter = false;
+
+    private void Start()
     {
-        // Check if the entering object is the TargetObject.
-        if (other.gameObject.CompareTag("Player")) // Make sure to set a unique tag for your TargetObject.
-        {
-            // Play the animation.
-            if (animator != null)
-            {
-                animator.SetTrigger("YourAnimationTriggerName");
-            }
+        // Hide the objectToShowHide and stop the particle emitter at the start
+        objectToShowHide.SetActive(false);
+        particleEmitter.Stop();
+    }
 
-            // Activate the particle emitter.
-            if (particleEmitter != null)
+    bool IsCameraWithinObjectPerimeter()
+    {
+        // Get the camera's position in world coordinates (ignoring y-axis).
+        Vector3 cameraPosition = oculusMainCamera.transform.position;
+        cameraPosition.y = 0f; // Ignore the y-axis.
+
+        // Check if the camera's x and z positions are within the specified boundaries.
+        bool isWithinBounds = (cameraPosition.x >= minX && cameraPosition.x <= maxX &&
+                               cameraPosition.z >= minZ && cameraPosition.z <= maxZ);
+
+        return isWithinBounds;
+    }
+
+    void Update()
+    {
+        bool isWithinPerimeter = IsCameraWithinObjectPerimeter();
+
+        // Check if the camera has entered or exited the perimeter
+        if (isWithinPerimeter != isInsidePerimeter)
+        {
+            isInsidePerimeter = isWithinPerimeter;
+
+            // Do something based on whether the camera is within the object's perimeter.
+            if (isInsidePerimeter)
             {
+                Debug.Log("Camera is within the object's perimeter.");
+                // Add your code here for when the camera is within the object.
+
+                objectToShowHide.SetActive(true);
                 particleEmitter.Play();
+            }
+            else
+            {
+                Debug.Log("Camera is NOT within the object's perimeter.");
+                // Add your code here for when the camera is NOT within the object.
+
+                objectToShowHide.SetActive(false);
+                particleEmitter.Stop();
             }
         }
     }
