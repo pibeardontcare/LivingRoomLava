744be8a727aff1b0dac963af7c65fada88c0cc9e
pibeardontcare <howtopoz@gmail.com>
2023-09-29 20:51:18 -0700

    Inventory Updates

--- Assets/InventoryManager.cs
+++ Assets/InventoryManager.cs
@@ -4,64 +4,37 @@ using UnityEngine;
 using Oculus;
 
 public class InventoryManager : MonoBehaviour
-{
-       // Reference to the left and right OVRHand components
-    public OVRHand leftHand;
-    public OVRHand rightHand;
 
-    public GameObject spawnedObject; // Reference to the spawned object
+{
+    public Transform oculusHand;  // Reference to the Oculus left hand GameObject
+    public GameObject objectPrefab;  // Reference to the object prefab to be spawned
+
+    private GameObject spawnedObject;
 
     private void Update()
     {
-        if (leftHand != null && leftHand.IsPointerPoseValid)
+        // Check if the Oculus hand GameObject exists
+        if (oculusHand != null)
         {
-            CheckAndSpawnObject(leftHand);
-        }
-        else if (rightHand != null && rightHand.IsPointerPoseValid)
-        {
-            CheckAndSpawnObject(rightHand);
-        }
-        else if (spawnedObject != null)
-        {
-            // If neither hand has a valid pointer pose, destroy the spawned object
-            Destroy(spawnedObject);
-            spawnedObject = null;
-        }
-    }
+            // Calculate the spawn position based on the hand's position and orientation
+            Vector3 spawnPosition = oculusHand.position + oculusHand.forward * 0.03f
+            - oculusHand.right * 0.05f   // Move -0.05 units in the x (towards fingers/palm)
+            + oculusHand.up * 0.05f;     // Move 0.05 units in the y (around the palm)
 
-    private void CheckAndSpawnObject(OVRHand hand)
-    {
-        // Get the orientation of the pointer pose (usually index finger)
-        Quaternion pointerPoseRotation = hand.PointerPose.rotation;
 
-        // Define a threshold for what constitutes "facing the user"
-        float facingThreshold = 0.9f;
-
-        // Check if the palm is facing the user
-        if (Vector3.Dot(pointerPoseRotation * Vector3.forward, Vector3.forward) > facingThreshold)
-        {
-            // Palm is facing the user, spawn the object if it hasn't been spawned yet
+            // Spawn the object if it doesn't exist
             if (spawnedObject == null)
             {
-                // Instantiate the object prefab at the hand's position and orientation
-                spawnedObject = Instantiate(spawnedObject, hand.PointerPose.position, pointerPoseRotation);
-
-                // Set the hand as the parent of the spawned object
-                spawnedObject.transform.parent = hand.transform; // The hand GameObject becomes the parent
-
-                // You can also adjust the object's local position and rotation relative to the hand if needed
-                // spawnedObject.transform.localPosition = ...
-                // spawnedObject.transform.localRotation = ...
+                spawnedObject = Instantiate(objectPrefab, spawnPosition, Quaternion.identity);
+            }
+            else
+            {
+                // Update the object's position to follow the hand
+                spawnedObject.transform.position = spawnPosition;
             }
         }
-        else
         {
-            // Palm is not facing the user, destroy the spawned object if it exists
-            if (spawnedObject != null)
-            {
-                Destroy(spawnedObject);
-                spawnedObject = null;
-            }
+            Debug.Log("Oculus hand GameObject not found!");
         }
     }
 }
