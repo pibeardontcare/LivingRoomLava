31f2e6c60bcd894ff465e698d97e0989c84df616
pibeardontcare <howtopoz@gmail.com>
2023-09-30 21:08:44 -0700

    Inventory debugging

--- Assets/InventoryManager.cs
+++ Assets/InventoryManager.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Oculus;
+using UnityEngine.UI;
+using TMPro;
 
 public class InventoryManager : MonoBehaviour
 
@@ -9,13 +11,32 @@ public class InventoryManager : MonoBehaviour
     public Transform oculusHand;  // Reference to the Oculus left hand GameObject
     public GameObject objectPrefab;  // Reference to the object prefab to be spawned
 
+    public TMP_Text debugText;
     private GameObject spawnedObject;
+
+
+  private void Start()
+    {
+        // Get the Text component on the Text object
+        debugText = GetComponent<TMP_Text>();
+    }
 
     private void Update()
     {
+
         // Check if the Oculus hand GameObject exists
         if (oculusHand != null)
         {
+
+            // Calculate the angle between the hand's up direction and the world up direction (palm orientation)
+            float angle = Vector3.Angle(oculusHand.up, Vector3.up);
+
+            // Update the Text object with the debug message
+            debugText.text = "mep";  // Format to two decimal places
+            // Calculate the spawn position based on the hand's position and orientation
+            
+           
+       
             // Calculate the spawn position based on the hand's position and orientation
             Vector3 spawnPosition = oculusHand.position + oculusHand.forward * 0.03f
             - oculusHand.right * 0.05f   // Move -0.05 units in the x (towards fingers/palm)
@@ -36,5 +57,7 @@ public class InventoryManager : MonoBehaviour
         {
             Debug.Log("Oculus hand GameObject not found!");
         }
+          
+            
     }
 }
