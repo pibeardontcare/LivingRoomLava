4539f0ae7020e1f5dd1741c6ab539656271f1460
pibeardontcare <howtopoz@gmail.com>
2023-10-02 20:37:22 -0700

    Collider hand tests

--- Assets/Scripts/PrizeGlowSelected.cs
+++ Assets/Scripts/PrizeGlowSelected.cs
@@ -7,9 +7,14 @@ using Oculus;
 
 public class PrizeGlowSelected : MonoBehaviour
 {
-
-   
-
-      
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Hand")) // Assuming "Hand" is tagged appropriately
+        {
+            // Hide or deactivate the object when the "Hand" enters the trigger collider
+            gameObject.SetActive(false);
+            // You can also destroy the object if you don't need it anymore
+            // Destroy(gameObject);
+        }
     }
-
+}
