af857e7a6b70ca22e41910205b62e2a2fb7f09b7
pibeardontcare <howtopoz@gmail.com>
2023-10-18 20:21:23 -0700

    Respawn Updates

--- Assets/SpawnPointManager.cs
+++ Assets/SpawnPointManager.cs
@@ -1,33 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class SpawnPointManager : MonoBehaviour
 {
+    public Transform spawnPoint; // Reference to spawn point Transform
+    public Camera oculusCamera; // Reference Oculus main camera
 
-    public Transform spawnPoint; // Reference to your spawn point Transform
-    public Camera oculusCamera; // Reference to your Oculus main camera
+    public GameObject objectToShow;
 
-    void Start()
+
+ private void Awake()
+
+
     {
-        if (oculusCamera == null)
+           // Show the object
+        objectToShow.SetActive(true);
+        // Your initializatio if (oculusCamera == null)
         {
             Debug.LogError("Oculus main camera not assigned. Camera may not spawn correctly.");
             return;
         }
 
-        if (spawnPoint != null)
+        // Check if the scene is the "MainMenu" scene
+        if (SceneManager.GetActiveScene().name == "MainMenu1")
         {
-            // Move the Oculus camera to the spawn point's position and rotation
-            oculusCamera.transform.position = spawnPoint.position;
-            oculusCamera.transform.rotation = spawnPoint.rotation;
-        }
-        else
-        {
-            Debug.LogError("Spawn point not assigned. Camera may not spawn correctly.");
+            if (spawnPoint != null)
+            {
+                // Move the Oculus camera to the spawn point's position and rotation
+                oculusCamera.transform.position = spawnPoint.position;
+                oculusCamera.transform.rotation = spawnPoint.rotation;
+            }
+            else
+            {
+                Debug.LogError("Spawn point not assigned. Camera may not spawn correctly.");
+            }
         }
     }
+
+    void Start()
+
+    {
+
+       
+    }
 }
-
-
-
