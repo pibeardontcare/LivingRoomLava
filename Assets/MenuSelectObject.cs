475f135d3a266d50455615b5c669ff3d89d6df75
pibeardontcare <howtopoz@gmail.com>
2023-12-03 19:45:07 -0800

    Menu Updates/fixes

--- Assets/MenuSelectObject.cs
+++ Assets/MenuSelectObject.cs
@@ -2,26 +2,73 @@ using UnityEngine;
 
 public class MenuSelectObject : MonoBehaviour
 {
-    public Material hoverMaterial; // Material to apply when hovered
-    private Material originalMaterial; // Original material of the object
+    public Material hoverMaterial; // Material to apply when collided
+    private Material[] originalMaterials; // Original materials of the objects
+    private Renderer[] objectRenderers; // Renderers of the objects
+
+    public GameObject[] objectsToChangeColor; // Array of objects to change color
 
     void Start()
     {
-        originalMaterial = GetComponent<Renderer>().material; // Assuming the object has a Renderer component
-    }
+        // Initialize arrays
+        objectRenderers = new Renderer[objectsToChangeColor.Length];
+        originalMaterials = new Material[objectsToChangeColor.Length];
 
-    // Function to apply hover material
-    public void ApplyHoverMaterial()
-    {
-        if (hoverMaterial != null)
+        // Store original materials for specified objects (only needs to be done once)
+        for (int i = 0; i < objectsToChangeColor.Length; i++)
         {
-            GetComponent<Renderer>().material = hoverMaterial;
+            objectRenderers[i] = objectsToChangeColor[i].GetComponent<Renderer>();
+
+            if (objectRenderers[i] != null)
+            {
+                originalMaterials[i] = objectRenderers[i].material;
+            }
+            else
+            {
+                Debug.LogError("Renderer component not found on object " + i);
+            }
         }
     }
 
-    // Function to reset material to original
-    public void ResetMaterial()
+    void OnCollisionEnter(Collision collision)
     {
-        GetComponent<Renderer>().material = originalMaterial;
+        // Check if the collision is with the specified collider
+        if (collision.gameObject.CompareTag("paint"))
+        {
+            ApplyHoverMaterialToSpecified();
+            Debug.Log("paintbrush hit");
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        // Check if the collision is with the specified collider
+        if (collision.gameObject.CompareTag("paint"))
+        {
+            ResetMaterialToOriginalSpecified();
+            Debug.Log("paintbrush exit");
+        }
+    }
+
+    private void ApplyHoverMaterialToSpecified()
+    {
+        for (int i = 0; i < objectRenderers.Length; i++)
+        {
+            if (objectRenderers[i] != null && hoverMaterial != null)
+            {
+                objectRenderers[i].material = hoverMaterial;
+            }
+        }
+    }
+
+    private void ResetMaterialToOriginalSpecified()
+    {
+        for (int i = 0; i < objectRenderers.Length; i++)
+        {
+            if (objectRenderers[i] != null)
+            {
+                objectRenderers[i].material = originalMaterials[i];
+            }
+        }
     }
 }
